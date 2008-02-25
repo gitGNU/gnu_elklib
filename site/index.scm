d2164db47bc317f7ec20b5109671ba62a5b88a8e
Alessandro Massignan <ff0000.it@gmail.com>
2008-02-25 19:03:59 +0100

    Updated site related files

--- site/index.scm
+++ site/index.scm
@@ -1,18 +1,21 @@
 (h3 "DESCRIPTION")
-(p "ELKLib is an Embeddable Library for Kernels development"
-
+(p "Embeddable Library for Kernel-level development."
+   (br)
+   "ELKlib should provide basic C/C++ support (string/memory functions,
+    STL-like containers and so on) for kernel developers. A sort of
+    uClibc/newlib oriented to the kernel-level development."
+ )
 (h3 "AUTHORS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
-
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+ )
 (h3 "RELEASE")
 (p "Sorry, no public release available at the moment.")
 (h3 "DEVELOPMENT")
-
 (p "You can Browse the "
    (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=elklib.git"))
       "Git repository")
-   " of this project with your web browser. This gives you a good picture
-    of the current status of the source files. You may also view the
-    complete histories of any file in the repository as well as
+   " of this project with your web browser. This gives you a good
+    picture of the current status of the source files. You may also view
+    the complete histories of any file in the repository as well as
     differences among two versions."
  )
