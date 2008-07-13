753316b55928ba318168d9ffe3379c8d43b9cf18
Francesco Salvestrini <salvestrini@gmail.com>
2008-07-14 00:38:27 +0200

    Updated site related file

--- site/in/index.scm
+++ site/in/index.scm
@@ -1,10 +1,37 @@
 (h3 "DESCRIPTION")
-(p
- "ELKlib is an embeddable library that provides C/C++ support oriented "
- "to kernel level development. ELKlib mimics GNU libc and the Standard "
- "Template Library"
- )
-(h4 "Copying")
+(p "ELKlib is an embeddable library that provides C/C++ support oriented "
+   "to kernel level development. ELKlib mimics GNU libc and the Standard "
+   "Template Library.")
+(br)
+(p "The library is under heavy development and is not yet usable, from the "
+   "user point of view.")
+
+;(h4 "Features")
+;(p "The libraries provides the following features (not updated):"
+;   (ul
+;    (li "C Support:")
+;    (ul 
+;     (li "The usual functions / macros available from ")
+;     (li "ctype.h")
+;     (li "assert.h")
+;     (li "stdio.h")
+;     (li "stdlib.h")
+;     (li "string.h")
+;     (li "unistd.h")
+;     (li "malloc.h")
+;     )
+;    (li "C++ Support:")
+;    (ul 
+;     (li "iterators")
+;     (li "algorithms")
+;     (li "vectors")
+;     (li "lists")
+;     (li "strings")
+;     )
+;    )
+;   )
+
+(h3 "Copying")
 (p
  "ELKlib is licensed under the "
  (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
@@ -25,24 +52,23 @@
 (p "Sorry, no public release available at the moment.")
 
 (h3 "MAILING LISTS")
-(p
- "ELKlib has several moderated mailing lists, each with an archive.
- For general ELKlib discussions, use <elklib-user AT nongnu.org>.
- Email bug reports to <elklib-bug AT nongnu.org>. For more information on
- submitting bugs, please see the section Report a Bug below.
- If you have a patch for a bug in ELKlib that hasn't yet been fixed in the
- latest repository sources, please send the patch (made for the git sources,
- not the release sources) to <elklib-patch AT nongnu.org>."
- )
+(p "ELKlib has a single moderated mailing list, with an archive. "
+   "In order to post a message to the mailing list you must be subscribed. "
+   "Please consult the "
+   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/elklib-generic"))
+      "ELKlib mailing list page")
+   " for more information on subscribing to the mailing list.")
 
 (h3 "REPORT A BUG")
-(p
- "If you think you have found a bug in ELKlib, then please send as complete
- a report as possible to <elklib-bug AT nongnu.org>. An easy way to collect all
- the required information, such as platform and compiler, is to run make check,
- and include the resulting file tests/testsuite.log to your report.
- Disagreements between the manual and the code are also bugs."
- )
+(p "If you think you have found a bug in ELKlib then please send as complete "
+   "a report as possible to <elklib AT nongnu.org>. An easy way to collect all "
+   "the required information, such as platform and compiler, is to include "
+   "in your report the config.log file available at the end of the "
+   "configuration procedure. ")
+(br)
+(p "If you have a patch for a bug in Hybrid that hasn't yet been fixed in the "
+   "latest repository sources, please be so kind to create it using the "
+   "repository sources, not the release sources.")
 
 (h3 "DEVELOPMENT")
 (h4 "Browsing sources")
